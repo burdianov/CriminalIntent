e0a61513049168e943b9ecc9e65e9425f7d92274
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-16 19:53:25 +0400

    Create abstract class SingleFragmentActivity

--- app/src/main/java/com/testography/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/testography/criminalintent/SingleFragmentActivity.java
@@ -1,14 +1,17 @@
 package com.testography.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
@@ -16,7 +19,7 @@ public class CrimeActivity extends FragmentActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
